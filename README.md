ac2e89dffb9fe34b57701eabeb2423ddd2fc8deb
bobo <1055271769@qq.com>
2020-11-12 17:30:55 +0800

    update v2.0

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 ## 说明
 - 非官方(官方Paddle版暂未开源)
+- 2020.11.12  基于Pytorch官方ResNet，更省内存，且支持更多网络
+- 2020.11.11  [简单版本](https://github.com/bobo0810/HS-ResNet/blob/231da98b98e0568af8a42bd4d36507bec97d4c30/hs_resnet.py)，内存占用稍高
 
 ## 环境
 
@@ -10,10 +12,18 @@
 |------------|-------------|--------|
 | 3.6        | 1.6.0       | Ubuntu |
 
-## 使用
+## 支持网络
+- hs_resnet50
+- hs_resnet101
+- hs_resnet152
+- hs_resnext50_32x4d
+- hs_resnext101_32x8d
+- hs_wide_resnet50_2
+- hs_wide_resnet101_2
 
+## 使用
 ```python
-import hs_resnet50,hs_resnet101,hs_resnet152
+import hs_resnet50
 pred=hs_resnet50(imgs)
 ```
 
---
+++ hsresnet/hs_block.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+class HSBlock(nn.Module):
+    '''
+    替代3x3卷积
+    '''
+    def __init__(self, in_ch, s=8):
+        '''
+        特征大小不改变
+        :param in_ch: 输入通道
+        :param s: 分组数
+        '''
+        super(HSBlock, self).__init__()
+        self.s = s
+        self.module_list = nn.ModuleList()
+        # 避免无法整除通道数
+        in_ch, in_ch_last = (in_ch // s, in_ch // s) if in_ch % s == 0 else (in_ch // s + 1, in_ch % s)
+        self.module_list.append(nn.Sequential())
+        acc_channels = 0
+        for i in range(1,self.s):
+            if i == 1:
+                channels=in_ch
+                acc_channels=channels//2
+            elif i == s - 1:
+                channels = in_ch_last + acc_channels
+            else:
+                channels=in_ch+acc_channels
+                acc_channels=channels//2
+            self.module_list.append(self.conv_bn_relu(in_ch=channels, out_ch=channels))
+        self.initialize_weights()
+
+    def conv_bn_relu(self, in_ch, out_ch, kernel_size=3, stride=1, padding=1):
+        conv_bn_relu = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
+        return conv_bn_relu
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def forward(self, x):
+        x = list(x.chunk(chunks=self.s, dim=1))
+        for i in range(1, len(self.module_list)):
+            y = self.module_list[i](x[i])
+            if i == len(self.module_list) - 1:
+                x[0] = torch.cat((x[0], y), 1)
+            else:
+                y1, y2 = y.chunk(chunks=2, dim=1)
+                x[0] = torch.cat((x[0], y1), 1)
+                x[i + 1] = torch.cat((x[i + 1], y2), 1)
+        return x[0]
